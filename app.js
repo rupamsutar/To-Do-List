f207f237cbd1c92079ae9d180504e40e7c305429
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-23 11:59:50 +0530

    Applied new CSS to form and paragraphs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-var items =[];
+var items =["Buy Food", "Cook Food", "Eat Food"];
 app.get("/", function(req,res) {
   var today = new Date();
   var options = {
